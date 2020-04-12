3e2594a5f4f1b2bc8abf0242ab397be9310eb22d
Spectrar <spectra1947@gmail.com>
2020-04-12 11:49:08 -0600

    HW turn in

---
+++ BlabberApp-master/BlabberApp.DataStoreTest/BlabAdapterMySqlTest.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BlabberApp.DataStore.Adapters;
+using BlabberApp.DataStore.Plugins;
+using BlabberApp.Domain.Entities;
+
+namespace BlabberApp.DataStoreTest
+{
+    [TestClass]
+    public class BlabAdapter_MySql_UnitTests
+    {
+        private BlabAdapter _harness = new BlabAdapter(new MySqlBlab());
+
+        [TestMethod]
+        public void Canary()
+        {
+            Assert.AreEqual(true, true);
+        }
+
+        [TestMethod]
+        public void TestAddAndGetBlab()
+        {
+            //Arrange
+            string email = "test@test.com";
+            User mockUser = new User(email);
+            Blab blab = new Blab("My test of blab.", mockUser);
+            //Act
+            _harness.Add(blab);
+            ArrayList actual = (ArrayList)_harness.GetByUserId(email);
+            //Assert
+            Assert.AreEqual(1, actual.Count);
+            _harness.Remove(blab);
+        }
+    }
+}
